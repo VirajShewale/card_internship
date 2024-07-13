d5d6188fa67982fb37f8356808431bc65b6d8ce3
Viraj <virajshewale21@gamil.com>
2024-07-13 11:35:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import BasketballPlayerCard from './components/BasketballPlayerCard';
+// import Navbar from './components/navbar/Navbar';
+import lebron from '../src/assets/Lebron.jpg';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <BasketballPlayerCard
+        name="LeBron James"
+        image={lebron}
+        position="Forward"
+        stats={{ pointsPerGame: 25.4, assistsPerGame: 7.1,
+        reboundsPerGame: 10.5 }}
+      />
+      <Footer />
+
+    </>
   );
 }
 
